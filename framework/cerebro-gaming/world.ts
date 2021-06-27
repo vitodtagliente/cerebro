3d01f9bb38020fd7ec186e776deb9abd74d97a6e
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-06-27 19:20:43 +0200

    changes

--- framework/cerebro-gaming/world.ts
+++ framework/cerebro-gaming/world.ts
@@ -1,4 +1,4 @@
-import WorldMap from "./world_map";
+import WorldMap, { WorldMapId, InvalidWorldMapId } from "./world_map";
 
 
 export default class World
@@ -6,13 +6,22 @@ export default class World
     private static _main: World = null;
     public static get main(): World { return World._main; }
 
-    private _maps: Array<WorldMap>;
+    private _maps: Map<WorldMapId, WorldMap>;
 
     public constructor()
     {
         World._main = this;
-        this._maps = [];
+        this._maps = new Map<WorldMapId, WorldMap>();
     }
 
-    public get maps(): Array<WorldMap> { return this._maps; }
+    public get(mapId: WorldMapId): WorldMap
+    {
+        if (mapId == InvalidWorldMapId) return null;
+
+        if (this._maps.has(mapId) == false)
+        {
+            this._maps.set(mapId, new WorldMap(mapId));
+        }
+        return this._maps[mapId];
+    }
 }
