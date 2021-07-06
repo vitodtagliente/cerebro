1c5d71684b7016e1ddb151549c06fbbdc3067bde
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2021-07-06 09:26:24 +0200

    Update time_map.ts

--- framework/cerebro-netcore/time_map.ts
+++ framework/cerebro-netcore/time_map.ts
@@ -14,30 +14,36 @@ class TimeValue<T>
 export default class TimeMap<K, V>
 {
     private _data: Map<K, TimeValue<V>>;
+    private _lifetime: number;
 
-    public constructor()
+    public constructor(lifetime: number = 1000)
     {
         this._data = new Map<K, TimeValue<V>>();
+        this._lifetime = lifetime;
     }
 
     public set(key: K, value: V): void
     {
-
+        this._data.set(key, new TimeValue<V>(value, this._lifetime));
     }
 
     public has(key: K): boolean
     {
-
-        return false;
+        return this._data.has(key);
     }
 
     public get(key: K): V
     {
+        const value: TimeValue<V> = this._data.get(key);
+        if (value)
+        {
+            return value.value;
+        }
         return null;
     }
 
     public delete(key: K): boolean
     {
-        return true;
+        return this._data.delete(key);
     }
 }
