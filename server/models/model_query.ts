2c4486413611c190acb3f873442f1c683fec4b73
Vito Domenico Tagliente <vitodomenicotagliente@gmail.com>
2022-08-27 10:41:04 +0200

    Update model_query.ts

--- server/models/model_query.ts
+++ server/models/model_query.ts
@@ -5,12 +5,7 @@ export default class ModelQuery
 {
     public static async all<T extends Model>(constr: { new(...args: any[]): T }, table: string): Promise<Array<T>>
     {
-        const result: Array<T> = [];
-        const entries = await Database.main.table(table).find({}).toArray();
-        for (const entry of entries)
-        {
-            result.push(Model.decode(constr, entry));
-        }
-        return result;
+        return (await Database.main.table(table).find({}).toArray())
+            .map(x => Model.decode(constr, x));
     }
 }
